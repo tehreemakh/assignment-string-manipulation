b3e3b81919da165d358de1d1eff918461c7d1d95
tehreemakh <149552853+tehreemakh@users.noreply.github.com>
2023-12-01 11:42:33 +0500

    Add files via upload

---
+++ replacing character from string.vb
@@ -0,0 +1,48 @@
+Imports System
+Imports System.IO
+Imports System.Runtime.ConstrainedExecution
+
+Module Program
+    Sub Main()
+        ' replacing character from string with another character.
+        'declaration of variables
+        Dim Str1, Str2 As String
+        Dim Char1, Char2, Char3 As Char
+        Dim i As Integer
+
+        'initialization
+        Str1 = ""
+        Str2 = ""
+
+        'input variables
+        Console.Write("enter the string: ")
+        Str1 = Console.ReadLine()
+        Console.Write("enter character to replace: ")
+        Char1 = Console.ReadLine()
+        Console.Write("enter character to replace with: ")
+        Char2 = Console.ReadLine()
+
+        'process
+        For i = 1 To Len(Str1)
+            Char3 = Mid(Str1, i, 1)
+            If Char1 <> Char3 Then
+                Str2 = Str2 + Char3
+            Else Str2 = Str2 + Char2
+            End If
+        Next
+
+        'output
+        Console.WriteLine("final string is: " + Str2)
+        Console.ReadLine()
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Module
