809599084b18c97d7eda708630fc61ec364b1805
tehreemakh <149552853+tehreemakh@users.noreply.github.com>
2023-12-01 11:46:33 +0500

    Update panagram.vb

--- panagram.vb
+++ panagram.vb
@@ -2,26 +2,22 @@ Imports System
 
 Module Program
     Sub Main()
-
-        
-   
+      
+        'declaration of variables
         Dim Str1, Alpha As String
         Dim Char1 As Char
         Dim i As Integer
         Dim IsPanagram As Boolean
     
-    
+        ' initialization
         Str1 = ""
         Alpha = ""
 
-
-
+        ' input and processing
         Console.Write("Enter the string: ")
         Str1 = UCase(Console.ReadLine)
         Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         IsPanagram = True
-
-
         For i = 1 To Len(Alpha)
             Char1 = Mid(Alpha, i, 1)
             If InStr(Str1, Char1) = 0 Then
@@ -29,7 +25,9 @@ Module Program
                 Exit For
             End If
         Next
-        If IsPanagram Then
+       
+    'output suitable prompts
+    If IsPanagram Then
             Console.Write("all alphabets are present in the string")
         Else Console.Write("all alpahabets are not present in the string")
         End If
